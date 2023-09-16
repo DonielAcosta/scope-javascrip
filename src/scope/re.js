052bd561acd5ea0fab696a2bd52e7282a15cfa3d
Doniel Acosta <donielacosta1995@gmail.com>
2023-09-15 23:24:34 -0400

    .

---
+++ src/scope/re.js
@@ -0,0 +1,17 @@
+var firstName; //undefined
+firstName = 'Doniel';
+console.log(firstName);
+
+var lastName = 'Alberto'; //declarar /asignar
+lastName = 'DUBEXY'; // reasignar 
+console.log(lastName);
+
+var secondName = 'Alberto';
+var secondName = 'Doniel';
+console.log(secondName);
+
+//let
+let fruta = 'coco';
+fruta = 'mamon';
+
+
