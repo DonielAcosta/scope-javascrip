103b271e30445109661333f5690e106f1802ab27
doniel <donielacosta1995@gmail.com>
2023-09-16 12:37:12 -0400

    reasignar

--- src/scope/re.js
+++ src/scope/re.js
@@ -1,17 +1,29 @@
 var firstName; //undefined
-firstName = 'Doniel';
+firstName = "Doniel";
 console.log(firstName);
 
-var lastName = 'Alberto'; //declarar /asignar
-lastName = 'DUBEXY'; // reasignar 
+var lastName = "Alberto"; //declarar /asignar
+lastName = "DUBEXY"; // reasignar
 console.log(lastName);
 
-var secondName = 'Alberto';
-var secondName = 'Doniel';
+var secondName = "Alberto";
+var secondName = "Doniel";
 console.log(secondName);
 
 //let
-let fruta = 'coco';
-fruta = 'mamon';
+let fruta = "coco";
+fruta = "mamon"; // reasignar
 
+console.log(fruta);
 
+let fruta = "mango";
+console.log(fruta);
+
+//const
+const animal = "dog"; // asignacion
+animal = "delfin"; // reasignacion
+console.log(animal);
+
+const autos = [];
+autos.push("toyota");
+console.log(autos);
