7cb2c2c0734d3b5f4815b690c31b89b0eb27e1c3
doniel <donielacosta1995@gmail.com>
2023-09-30 17:25:21 -0400

    mejoras y introduccion

--- src/clousure/closure.js
+++ src/clousure/closure.js
@@ -1,24 +1,69 @@
-const moneyBox = (coins) =>{
-    var saveCoins = 0;
-    saveCoins += coins;
-    console.log(`moneyBox: ${saveCoins}`);
-}
+// const moneyBox = (coins) =>{
+//     var saveCoins = 0;
+//     saveCoins += coins;
+//     console.log(`moneyBox: ${saveCoins}`);
+// }
 
-moneyBox(5);
-moneyBox(10);
+// moneyBox(5);
+// moneyBox(10);
 
 
-const moneyBox = () =>{
-    var saveCoins = 0;
-    const countCoins = (coins) =>{
+// const moneyBox = () =>{
+//     var saveCoins = 0;
+//     const countCoins = (coins) =>{
+//         saveCoins += coins;
+//         console.log(`moneyBox: ${saveCoins}`);
+//     }
+//     return countCoins;
+// }
+
+// let mymoneyBox = moneyBox();
+
+// mymoneyBox(4);
+// mymoneyBox(5);
+// mymoneyBox(6);
+
+// function greeting(){
+//     let username = 'Doniel';
+
+//     function displayUserName(){
+//         return `Hello ${username}`;
+//     }
+
+//     return displayUserName;
+// }
+// const g = greeting();
+// console.log(g);
+// console.log(g());
+
+// function moneyBox(coins){
+//     let saveCoins = 0;
+//     saveCoins += coins;
+//     console.log(`MoneyBox: $${saveCoins}`);
+// }
+
+// moneyBox(5);
+// moneyBox(5);
+
+function moneyBox(){
+    let saveCoins = 0;
+    function countCoins(coins){
         saveCoins += coins;
-        console.log(`moneyBox: ${saveCoins}`);
+        console.log(`MoneyBox: $${saveCoins}`);
+
     }
     return countCoins;
 }
 
-let mymoneyBox = moneyBox();
+const mymoneyBox = moneyBox();
+mymoneyBox(50);
+mymoneyBox(50);
+mymoneyBox(100);
 
-mymoneyBox(4);
-mymoneyBox(5);
-mymoneyBox(6);
+
+const moneyBoxAna = moneyBox();
+
+moneyBoxAna(500);
+moneyBoxAna(3);
+moneyBoxAna(2);
+
--- src/clousure/lexical.js
+++ src/clousure/lexical.js
@@ -15,3 +15,24 @@ const myOtherCount = buildCount(10);
 myOtherCount();
 myOtherCount();
 myOtherCount();
+
+const myGlobal = 0;
+
+function myFunction(){
+    const myNumber = 1;
+    console.log(myGlobal);
+
+
+    function parent(){// funcion interna 
+        const inner = 2;
+        console.log(myNumber,myGlobal);
+
+        function child(){
+            console.log(inner,myNumber,myGlobal);
+        }
+        return child();
+    }
+    return parent();
+}
+
+myFunction();
